c1ed70efe66644fd597c92ebcd31225b1df92e9c
rafael.maced0 <rafael.macedo@softplan.com.br>
2022-02-04 17:00:00 -0300

    tests: add more tests to controller

--- test/dev_cilada_web/controllers/classifier_controller_test.exs
+++ test/dev_cilada_web/controllers/classifier_controller_test.exs
@@ -7,8 +7,40 @@ defmodule DevCiladaWeb.ClassifierControllerTest do
 
   describe "show" do
     test "show given classifier", %{conn: conn} do
-      conn = get(conn, Routes.classifier_path(conn, :show, "123"))
-      assert json_response(conn, 200)["data"] != []
+      conn = get(conn, Routes.classifier_path(conn, :show, "hash_classifier"))
+
+      assert json_response(conn, 200)["data"] == %{
+               "hash" => "hash_classifier",
+               "cilada_threshold" => 10
+             }
+    end
+  end
+
+  describe "create" do
+    test "create classifier", %{conn: conn} do
+      classifier = %{"classifier" => %{"cilada_threshold" => 15}}
+
+      conn = post(conn, Routes.classifier_path(conn, :create, classifier))
+
+      assert json_response(conn, 200)["data"] == %{
+               "hash" => 1234,
+               "cilada_threshold" => "15"
+             }
+    end
+  end
+
+  describe "classsify" do
+    test "classify job proposal", %{conn: conn} do
+      job_proposal = %{
+        "job_proposal" => %{"perks" => ["123", "456"], "classifier" => "hash_classifier"}
+      }
+
+      conn = post(conn, Routes.classifier_path(conn, :classsify, job_proposal))
+
+      assert json_response(conn, 200)["data"] == %{
+               "perks" => ["123", "456"],
+               "is_cilada" => true
+             }
     end
   end
 end

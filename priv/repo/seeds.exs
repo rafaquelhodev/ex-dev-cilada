9d7c3814e08c90b707175878f11f833edce79877
rafael.maced0 <rafael.macedo@softplan.com.br>
2022-02-08 09:10:30 -0300

    aux: creating seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,12 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias DevCilada.Cilada
+
+classifier = %{"cilada_threshold" => 40}
+perk_01 = %{"cilada_points" => 5, "description" => "aqui tem cafézinho"}
+perk_02 = %{"cilada_points" => 15, "description" => "brilho nos olhos"}
+perk_03 = %{"cilada_points" => 30, "description" => "pode vir de bermuda"}
+
+Cilada.create_classifier(classifier, [perk_01, perk_02])
